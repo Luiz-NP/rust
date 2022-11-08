4c70205a1504f1e2cf21dc80df10a58167e0d21b
Luiz <gustavoooliveira111@gmail.com>
2022-11-08 04:41:24 -0300

    Comparing the Guess to the Secret Number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -20,6 +20,12 @@ fn main() {
     let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
     println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
 
 
