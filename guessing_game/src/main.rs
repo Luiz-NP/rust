00efa7d541af56f35578cfe270f523b25d7415ae
Luiz <gustavoooliveira111@gmail.com>
2022-11-08 04:38:04 -0300

    adding guess convertion to u32

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -18,3 +18,55 @@ fn main() {
 
     println!("You guessed: {guess}");
 }
+
+use std::io;
+use rand::Rgn;
+
+fn main() {
+    println!("Guess The Number");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
+
+    println!("Please input your guess:");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    println!("You guessed: {guess}");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
