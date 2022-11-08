1007a5aac910bc5407dff2aa15968e0fd5e7442b
Luiz <gustavoooliveira111@gmail.com>
2022-11-08 04:40:20 -0300

    adding Ordering enum

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,26 +1,6 @@
 use std::io;
-use rand::Rng;
-
-fn main() {
-    println!("Guess the number!");
-
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-
-    println!("The secret number is: {secret_number}");
-
-    println!("Please input your guess.");
-
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    println!("You guessed: {guess}");
-}
-
-use std::io;
 use rand::Rgn;
+use std::cmp::Orgering;
 
 fn main() {
     println!("Guess The Number");
