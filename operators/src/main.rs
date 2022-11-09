84f5097c0186a067f49cda6f3b333c18b177182d
Luiz <gustavoooliveira111@gmail.com>
2022-11-09 10:25:17 -0300

    rust operators

---
+++ operators/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    // addition
+    let sum = 5 + 10;
+    println!("sum: {sum}");
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+    println!("difference: {difference}");
+
+    // multiplication
+    let product = 4 * 30;
+    println!("product: {product}");
+
+    // division
+    let quotient = 56.7 / 32.2;
+    let floored = 2 / 3; // Results in 0
+    println!("quotient: {quotient}");
+    println!("floored: {floored}");
+
+    // remainder
+    let remainder = 43 % 5;
+    println!("reminder: {remainder}");
+}
