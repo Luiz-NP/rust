f4b18fa9ccc3a053cdd1ea3758ad4adec00b171b
Luiz Gustavo <101659217+Luiz-NP@users.noreply.github.com>
2022-11-09 10:31:53 -0300

    Update main.rs

--- operators/src/main.rs
+++ operators/src/main.rs
@@ -1,23 +1,17 @@
 fn main() {
     // addition
-    let sum = 5 + 10;
-    println!("sum: {sum}");
-
+    let sum = 5 + 10; // 15
+    
     // subtraction
-    let difference = 95.5 - 4.3;
-    println!("difference: {difference}");
+    let difference = 95.5 - 4.3; // 91.2
 
     // multiplication
-    let product = 4 * 30;
-    println!("product: {product}");
+    let product = 4 * 30; // 120
 
     // division
-    let quotient = 56.7 / 32.2;
-    let floored = 2 / 3; // Results in 0
-    println!("quotient: {quotient}");
-    println!("floored: {floored}");
+    let quotient = 2.0 / 3.0; // 0.6666666666666666
+    let floored = 2 / 3; // 0
 
     // remainder
-    let remainder = 43 % 5;
-    println!("reminder: {remainder}");
+    let remainder = 43 % 5; // 3
 }
