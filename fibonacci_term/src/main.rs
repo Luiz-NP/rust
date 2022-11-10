7b473688769ba35b704d76788f9e4e63a53688e3
Luiz <gustavoooliveira111@gmail.com>
2022-11-10 11:03:20 -0300

    finding fibonacci terms in rust

--- fibonacci_term/src/main.rs
+++ fibonacci_term/src/main.rs
@@ -1,3 +1,23 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut n = String::new();
+
+    println!("Please enter a number greater than 0: ");
+
+    io::stdin().read_line(&mut n).expect("failed to read input");
+
+    let n: u32 = n.trim().parse().expect("Please type a number!");
+
+    println!("The {}° number of the fibonacci sequence is: {}", n, fibonacci(n));
+}
+
+fn fibonacci(n: u32) -> u32 {
+    if n == 1 {
+        return 0;
+    } else if n == 2 {
+        return 1;
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 }
